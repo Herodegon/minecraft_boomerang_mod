5a7d6ef359dce0b2c4b9a72c37a40618dba45bea
herodegon <herodegon@gmail.com>
2025-07-30 12:37:12 -0400

    Initial commit

--- src/main/java/net/herodegon/tutorialmod/TutorialMod.java
+++ src/main/java/net/herodegon/tutorialmod/TutorialMod.java
@@ -1,11 +1,14 @@
 package net.herodegon.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.herodegon.tutorialmod.entity.ModEntities;
+import net.herodegon.tutorialmod.item.ModItems;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TutorialMod implements ModInitializer {
+public class TutorialMod implements ModInitializer 
+{
 	public static final String MOD_ID = "tutorialmod";
 
 	// This logger is used to write text to the console and the log file.
@@ -14,11 +17,15 @@ public class TutorialMod implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
+	public void onInitialize() 
+	{
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		ModItems.initialize();
+		ModEntities.initialize();
 	}
 }
---
+++ src/main/java/net/herodegon/tutorialmod/entity/custom/BoomerangProjectileEntity.java
@@ -0,0 +1,65 @@
+package net.herodegon.tutorialmod.entity.custom;
+
+import net.herodegon.tutorialmod.entity.ModEntities;
+import net.herodegon.tutorialmod.item.ModItems;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.world.World;
+
+public class BoomerangProjectileEntity extends PersistentProjectileEntity
+{
+    private float rotation = 0.0f;
+    private static final float RANGE = 10.0f;
+    private static final float DAMAGE = 3.0f;
+
+    public BoomerangProjectileEntity(EntityType<? extends PersistentProjectileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    public BoomerangProjectileEntity(World world, PlayerEntity player)
+    {
+        super(ModEntities.BOOMERANG_ENTITY, player, world, new ItemStack(ModItems.BOOMERANG_ITEM), null);
+    }
+
+    @Override
+    public void tick()
+    {
+        super.tick();
+        float distance = (float) this.getPos().distanceTo(this.getOwner().getPos());
+        if (distance > RANGE)
+        {
+            setVelocity(getVelocity().multiply(-1.0, 0.0, -1.0));
+        }
+    }
+
+    @Override
+    protected ItemStack getDefaultItemStack()
+    {
+        return new ItemStack(ModItems.BOOMERANG_ITEM);
+    }
+
+    public float getRenderingRotation()
+    {
+        rotation += 0.5f;
+        if (rotation >= 360.0f)
+        {
+            rotation -= 360.0f;
+        }
+        return rotation;
+    }
+
+    @Override
+    protected void onEntityHit(EntityHitResult entityHitResult) {
+		super.onEntityHit(entityHitResult);
+		Entity entity = entityHitResult.getEntity();
+        if (this.getWorld() instanceof ServerWorld serverWorld)
+        {
+            entity.damage(serverWorld, this.getDamageSources().thrown(this, this.getOwner()), DAMAGE);
+        }
+	}
+}
